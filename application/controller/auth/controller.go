cf4e0b698d689ca61583685e5e5f0c4ac210e72d
rubensantoniorosa2704 <rubensrosaneto27@gmail.com>
2024-10-17 16:07:13 -0300

    wip

--- application/controller/auth/controller.go
+++ application/controller/auth/controller.go
@@ -20,15 +20,16 @@ func New() *Auth {
 
 // Login godoc
 // @Summary      Login
-// @Description  get user access token
+// @Description  Authenticates a user and returns an access token.
 // @Tags         accounts
 // @Accept       json
 // @Produce      json
 // @Param        id   path      int  true  "Account ID"
-// @Success      200  {object}  string
-// @Failure      400  {object}  string
-// @Failure      404  {object}  string
-// @Failure      500  {object}  string
+// @Param        body body      entities.User true "User credentials"
+// @Success      200  {object}  string "Access token"
+// @Failure      400  {object}  string "Bad request"
+// @Failure      404  {object}  string "Not found"
+// @Failure      500  {object}  string "Internal server error"
 // @Router       /api/v1/auth/login [post]
 func (controller Auth) Login(w http.ResponseWriter, r *http.Request) {
 	var requestBody entities.User
@@ -36,17 +37,15 @@ func (controller Auth) Login(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&requestBody)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
-
 		return
 	}
 
 	token, err := controller.service.Login(requestBody.Email, requestBody.Password)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-
 		return
 	}
 
-	w.Write([]byte(token))
 	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(token))
 }
