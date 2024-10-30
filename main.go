2f65dffea89452dc0cbc0410c604eae7251022ff
Rubens Rosa <rubensrosaneto27@gmail.com>
2024-10-30 20:07:18 -0300

    feat: adjust descriptions

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import controller "com.redberry.api/application"
 
+// @title Redberry API
+// @version 1.0
+// @description Project management system.
 // @securityDefinitions.apikey BearerAuth
 // @in header
 // @name Authorization
